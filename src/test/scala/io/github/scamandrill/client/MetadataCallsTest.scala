dfbab16cb6d70ddcf300bdbb81bd3d73a5b0d05c
Joel Sonoda <joelsonoda@gmail.com>
2016-06-02 14:51:00 -0600

    Added tests for the metadata calls

---
+++ src/test/scala/io/github/scamandrill/client/MetadataCallsTest.scala
@@ -0,0 +1,68 @@
+package io.github.scamandrill.client
+
+import io.github.scamandrill.MandrillSpec
+import io.github.scamandrill.models._
+
+class MetadataCallsTest extends MandrillSpec {
+
+  "List" should "handle the example at https://www.mandrillapp.com/api/docs/metadata.JSON.html#method=list" in {
+    withClient("/metadata/list.json") { wc =>
+      val instance = new MandrillClient(wc, new APIKey())
+      whenReady(instance.metadataList(), defaultTimeout)(_ shouldBe MandrillSuccess(List(
+        MIMetadataResponse(
+          name = "group_id",
+          state = "active",
+          view_template = "<a href=\"http://yourapplication.com/user/{{value}}\">{{value}}</a>"
+        )
+      )))
+    }
+  }
+
+  "Add" should "handle the example at https://www.mandrillapp.com/api/docs/metadata.JSON.html#method=add" in {
+    withClient("/metadata/add.json") { wc =>
+      val instance = new MandrillClient(wc, new APIKey())
+      whenReady(instance.metadataAdd(MMeteadatapAdd(
+        name = "group_id",
+        view_template= "<a href=\"http://yourapplication.com/user/{{value}}\">{{value}}</a>"
+      )), defaultTimeout)(_ shouldBe MandrillSuccess(
+        MIMetadataResponse(
+          name = "group_id",
+          state = "active",
+          view_template = "<a href=\"http://yourapplication.com/user/{{value}}\">{{value}}</a>"
+        )
+      ))
+    }
+  }
+
+  "Update" should "handle the example at https://www.mandrillapp.com/api/docs/metadata.JSON.html#method=update" in {
+    withClient("/metadata/update.json") { wc =>
+      val instance = new MandrillClient(wc, new APIKey())
+      whenReady(instance.metadataUpdate(MMeteadatapAdd(
+        name = "group_id",
+        view_template= "<a href=\"http://yourapplication.com/user/{{value}}\">{{value}}</a>"
+      )), defaultTimeout)(_ shouldBe MandrillSuccess(
+        MIMetadataResponse(
+          name = "group_id",
+          state = "active",
+          view_template = "<a href=\"http://yourapplication.com/user/{{value}}\">{{value}}</a>"
+        )
+      ))
+    }
+  }
+
+  "Delete" should "handle the example at https://www.mandrillapp.com/api/docs/metadata.JSON.html#method=delete" in {
+    withClient("/metadata/delete.json") { wc =>
+      val instance = new MandrillClient(wc, new APIKey())
+      whenReady(instance.metadataDelete(MMeteadatapDelete(
+        name = "group_id"
+      )), defaultTimeout)(_ shouldBe MandrillSuccess(
+        MIMetadataResponse(
+          name = "group_id",
+          state = "active",
+          view_template = "<a href=\"http://yourapplication.com/user/{{value}}\">{{value}}</a>"
+        )
+      ))
+    }
+  }
+}
+
