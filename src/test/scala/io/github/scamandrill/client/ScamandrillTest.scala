6e3476d6f8860d5d8b71fee1e5bcac365eb08365
Joel Sonoda <joelsonoda@gmail.com>
2016-09-08 10:48:18 -0600

    Add a test to make sure that Scamandrill can get the mandrill api key from application.conf

--- src/test/scala/io/github/scamandrill/client/ScamandrillTest.scala
+++ src/test/scala/io/github/scamandrill/client/ScamandrillTest.scala
@@ -5,7 +5,7 @@ import play.api.Configuration
 import play.api.libs.ws.ahc.AhcWSClient
 
 class ScamandrillTest extends MandrillSpec {
-  "Scamandrill" should "create a new WSClient using overridden values from application.Mandrill" in {
+  "Scamandrill" should "create a new WSClient using overridden values from supplied config" in {
     val instance = Scamandrill(Configuration(
       "play.ws.ahc.maxConnectionsPerHost" -> 5
     ))
@@ -14,6 +14,15 @@ class ScamandrillTest extends MandrillSpec {
         case AhcWSClient(underlying) => underlying.getMaxConnectionsPerHost shouldBe 5
         case _ => fail("The underlying client should be a AhcWSClient if scamandrill is constructed with no args")
       }
+    } finally {
+      instance.shutdown()
+    }
+  }
+
+  it should "use the key from application.conf if no config is specified" in {
+    val instance = Scamandrill()
+    try {
+      instance.get().key.key shouldBe "example key"
     } finally {
       instance.shutdown()
     }
